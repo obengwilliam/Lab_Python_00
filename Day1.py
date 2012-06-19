d8b8b4061f041d28d1ccecb14b91a9eec31fd41c
Guest <guest-lCFJrK@AITI-75.(none)>
2012-06-19 15:41:44 +0000

    lab instruction assignment

---
+++ Day1.py
@@ -0,0 +1,28 @@
+'''
+creator:OBENG WILLIAM
+description:day01 using python as a calcultor
+'''
+
+import math
+print '11/2 is',11/2,'\n'
+
+print '11.0/2 is',11.0/2,'\n'
+
+print '3 to the power 27 is ',3**27,'\n'
+
+print '134533 +6889980-84849 to the power of 3 is',(134533 +6889980-84849)**3
+
+#question 4
+
+distance_tomarket=7.9 * 1000 #meters
+print distance_tomarket
+time_towalk=45 # minute
+print 'The average time per meter is ',time_towalk/distance_tomarket,'minute/meters'
+
+
+
+#question 5
+distance_north_byfriend=4.7*0.621371 #miles
+distance_east_myself=9.4* 0.621371 #miles
+shortest_distance=(distance_north_byfriend **2)+(distance_east_myself**2)
+print 'The shortest distance is',math.sqrt(shortest_distance)
